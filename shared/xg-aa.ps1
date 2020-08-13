a724637aebe730386cb0b598a3e2a53b6c7510a4
Jorn Lutters <jorn.lutters@sophos.com>
2020-08-13 15:30:11 -0700

    Modified Azure Automation job to reflect issue found in xg-aa.ps1 boot script causing non-persistence over reboot

--- shared/xg-aa.ps1
+++ shared/xg-aa.ps1
@@ -28,13 +28,13 @@ If ($session.Connected) {
     Start-Sleep -s 5
     $SSHStream.WriteLine("mount -o remount,rw /")
     Start-Sleep -s 5
-    $SSHStream.WriteLine("sed -i `"2i echo '200 azurespecialroutes' >> /etc/iproute2/rt_tables`" /scripts/system/clientpref/customization_application_startup.sh")
+    $SSHStream.WriteLine("echo '200 azurespecialroutes' >> /etc/iproute2/rt_tables")
     Start-Sleep -s 5
-	$SSHStream.WriteLine("sed -i `"3i ip route add default via $portagw dev PortA table 200`" /scripts/system/clientpref/customization_application_startup.sh")
+	$SSHStream.WriteLine("sed -i `"2i ip route add default via $portagw dev PortA table 200`" /scripts/system/clientpref/customization_application_startup.sh")
     Start-Sleep -s 5
-    $SSHStream.WriteLine("sed -i `"4i ip rule add from $portaip to 168.63.129.16 table 200`" /scripts/system/clientpref/customization_application_startup.sh")
+    $SSHStream.WriteLine("sed -i `"3i ip rule add from $portaip to 168.63.129.16 table 200`" /scripts/system/clientpref/customization_application_startup.sh")
     Start-Sleep -s 5
-	$SSHStream.WriteLine("sed -i `"5i ip route flush cache`" /scripts/system/clientpref/customization_application_startup.sh")
+	$SSHStream.WriteLine("sed -i `"4i ip route flush cache`" /scripts/system/clientpref/customization_application_startup.sh")
 	Start-Sleep -s 5
 	$SSHStream.WriteLine("reboot")
     Start-Sleep -s 5
